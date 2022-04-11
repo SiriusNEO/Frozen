b08c63bceefe4c2dba6d6d37e6f997b6a8a617df
SiriusNEO <1713833595@qq.com>
2022-04-11 16:12:18 +0800

    update doc pic link

--- Frozen/src/main.cpp
+++ Frozen/src/main.cpp
@@ -5,9 +5,12 @@
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
+
     QApplication a(argc, argv);
+
     Widget w;
     w.setWindowTitle("Frozen!");
     w.show();
+
     return a.exec();
 }
